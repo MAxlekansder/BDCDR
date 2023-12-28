bf10de67e36e2b4016287b6f8fcd877d9b02b6f5
Alexander Hasslund <alexander.hasslund@stud.sti.se>
2023-12-28 11:29:14 +0100

    forgot window-query...

--- src/main/java/com/alexanderhasslund/demo/main/Engine/DatabaseHandler/DatabaseGetter.java
+++ src/main/java/com/alexanderhasslund/demo/main/Engine/DatabaseHandler/DatabaseGetter.java
@@ -30,31 +30,30 @@ public class DatabaseGetter {
                     "  limit 5";
            */
 
-            String highscorePlayer = "SELECT\n" +
-                    "    DISTINCT p.playerName,\n" +
-                    "    c.className,\n" +
-                    "    p2.`level`,\n" +
-                    "    p2.MonsterKilled,\n" +
-                    "    COUNT(battleId) AS BattleCount,\n" +
-                    "    IFNULL(m.HasPartyBeatenLevel, 0) AS tagteam,\n" +
-                    "    IFNULL(m2.HasPartyBeatenLevel, 0) AS twinBro,\n" +
-                    "    IFNULL(m3.HasPartyBeatenLevel, 0) AS inq,\n" +
-                    "    IFNULL(m4.HasPartyBeatenLevel, 0) AS thaal\n" +
-                    "FROM\n" +
-                    "    dungeonrun.player p\n" +
-                    "JOIN dungeonrun.playeractiveclass p2 ON p.PlayerId = p2.playerId\n" +
-                    "JOIN dungeonrun.class c ON c.classId = p2.ClassId\n" +
-                    "LEFT JOIN dungeonrun.maplevelcompleted m ON m.PlayerId = p.PlayerId AND m.MapId = 2\n" +
-                    "LEFT JOIN dungeonrun.maplevelcompleted m2 ON m2.PlayerId = p.PlayerId AND m2.MapId = 3\n" +
-                    "LEFT JOIN dungeonrun.maplevelcompleted m3 ON m3.PlayerId = p.PlayerId AND m3.MapId = 4\n" +
-                    "LEFT JOIN dungeonrun.maplevelcompleted m4 ON m4.PlayerId = p.PlayerId AND m4.MapId = 5\n" +
-                    "LEFT JOIN (\n" +
-                    "    SELECT DISTINCT playerId, battleId\n" +
-                    "    FROM dungeonrun.monsterplayerfight m5\n" +
-                    ") AS cc ON cc.playerId = p.playerId\n" +
-                    "-- other conditions and ordering remain the same\n" +
-                    "ORDER BY tagteam DESC, twinBro DESC, inq DESC, thaal DESC\n" +
-                    "LIMIT 5;";
+            String highscorePlayer = "SELECT DISTINCT p.playerName,\n" +
+                    "                 c.className,\n" +
+                    "                   p2.`level`,\n" +
+                    " p2.MonsterKilled,\n" +
+                    "\tCOUNT(battleId) OVER (PARTITION BY p.playerId) AS BattleCount,\n" +
+                    "     IFNULL(m.HasPartyBeatenLevel, 0) AS tagteam,\n" +
+                    " IFNULL(m2.HasPartyBeatenLevel, 0) AS twinBro,\n" +
+                    "IFNULL(m3.HasPartyBeatenLevel, 0) AS inq,\n" +
+                    " IFNULL(m4.HasPartyBeatenLevel, 0) AS thaal\n" +
+                    "    FROM\n" +
+                    "        dungeonrun.player p\n" +
+                    "    JOIN dungeonrun.playeractiveclass p2 ON p.PlayerId = p2.playerId\n" +
+                    "    JOIN dungeonrun.class c ON c.classId = p2.ClassId\n" +
+                    "    LEFT JOIN dungeonrun.maplevelcompleted m ON m.PlayerId = p.PlayerId AND m.MapId = 2\n" +
+                    "    LEFT JOIN dungeonrun.maplevelcompleted m2 ON m2.PlayerId = p.PlayerId AND m2.MapId = 3\n" +
+                    "    LEFT JOIN dungeonrun.maplevelcompleted m3 ON m3.PlayerId = p.PlayerId AND m3.MapId = 4\n" +
+                    "    LEFT JOIN dungeonrun.maplevelcompleted m4 ON m4.PlayerId = p.PlayerId AND m4.MapId = 5\n" +
+                    "   \tLEFT JOIN (\n" +
+                    "        SELECT DISTINCT playerId, battleId\n" +
+                    "        FROM dungeonrun.monsterplayerfight m5\n" +
+                    "    ) AS cc ON cc.playerId = p.playerId\n" +
+                    "    -- other conditions and ordering remain the same\n" +
+                    "    ORDER BY tagteam DESC, twinBro DESC, inq DESC, thaal DESC\n" +
+                    "    LIMIT 5;";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(highscorePlayer)) {
 
